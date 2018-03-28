ea2d6c93314dbce72a89c89afdcdd1c6f069f62b
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-28 12:02:18 +0100

    Add daily kw use calculation

--- js/main.js
+++ js/main.js
@@ -10,4 +10,6 @@ for(var i = 0; i < months.length; i++){
     monthlyUseKw = Number(months[i].value);
     annualUseKw += monthlyUseKw;
 }
-console.log(annualUseKw);
+dailyUseKw = Math.ceil(annualUseKw / 365);
+console.log('Annual use kw is: ',annualUseKw);
+console.log('Daily use kw is: ',dailyUseKw);
