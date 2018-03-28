ab0c1805c07d9b6555e86d9019623eb9cb314a38
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-28 12:28:16 +0100

    We calculate daily use in kw and the hours per day in the zone selected, after clicking on Solar Recomendation button.

--- js/main.js
+++ js/main.js
@@ -15,5 +15,43 @@ function addMonths(elem) {
     return dailyUseKw;
 }
 
-var dailyUseKw = addMonths('mpc');
-console.log(dailyUseKw);
+function sunHours() {
+    var hours;
+    var theZone = document.forms.solarForm.zone.selectedIndex;
+    theZone += 1; //We get from the zone selection
+// the option selected which is an index in the array 0-based
+// and we need to get zones from 1 to 6
+
+    switch (theZone) {
+        case 1:
+            hours = 6;
+            break;
+        case 2:
+            hours = 5.5;
+            break;
+        case 3:
+            hours = 5;
+            break;
+        case 4:
+            hours = 4.5;
+            break;
+        case 5:
+            hours = 4.2;
+            break;
+        case 6:
+            hours = 3.5;
+            break;
+        default:
+            hours = 0;
+    }
+    return (hours);
+}
+
+function calculateSolar() {
+    var dailyUseKw = addMonths('mpc');
+    console.log('Our daily use of kw: ', dailyUseKw);
+
+
+    var sunHoursPerDay = sunHours();
+    console.log('Our hours of sun per day are: ', sunHoursPerDay);
+}
