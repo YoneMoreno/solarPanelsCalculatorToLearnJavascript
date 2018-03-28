0f20baa67b91a4bca48d1f4fa7ec108725ccb2e6
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-28 11:59:36 +0100

    We calculate the annualKwUse

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 /*jslint browser:true */
 "use strict";
+
+var annualUseKw = 0, dailyUseKw = 0, monthlyUseKw = 0;
+
 var months = document.getElementById('mpc').getElementsByTagName('input');
 console.log(months);
+
+for(var i = 0; i < months.length; i++){
+    monthlyUseKw = Number(months[i].value);
+    annualUseKw += monthlyUseKw;
+}
+console.log(annualUseKw);
